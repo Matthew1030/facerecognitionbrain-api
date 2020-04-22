5c00da7f74da060bdfb3eafaa5dd5750b2c9570b
Matthew <mhisscock@gmail.com>
2020-04-22 06:32:06 -0500

    added image logging

--- Controllers/image.js
+++ Controllers/image.js
@@ -18,6 +18,7 @@ const handleApiCall = (req, res) => {
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
+    console.log(id);
     db('users').where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
