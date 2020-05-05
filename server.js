e623da554e9a7bd8c4995646e9565a05cf68f2c0
Matthew <mhisscock@gmail.com>
2020-05-05 14:32:22 -0500

    server.js improvements

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
